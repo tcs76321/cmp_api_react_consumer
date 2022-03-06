9120f0cef209f3e0ae9797f7a1f13df8d3bb0ae9
Trevor Stahl <trevorclarkstahl@gmail.com>
2022-03-06 15:46:00 -0700

    trying again

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+
+
 
 function App() {
+    const [results, setResults] = useState([])
+
+    fetch('http://localhost:5000/comp')
+        .then(response => response.json())
+        .then(data => {
+            setResults(data)
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+            {/*<p>{results}</p>*/}
+        </header>
+      </div>
   );
 }
 
