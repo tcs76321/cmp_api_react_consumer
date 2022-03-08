bf424883c0d0c7cc3a27fdeedc56ff90967fd6e4
Trevor Stahl <trevorclarkstahl@gmail.com>
2022-03-07 22:19:24 -0700

    add data file that will export json constants, fullpass done

---
+++ src/data.js
@@ -0,0 +1,72 @@
+export const dataJsonFileAllPass = [
+        {
+            metabolite: "albumin",
+            range: 35,
+            result: "unknown"
+        },
+        {
+            metabolite: "bun",
+            range: 200,
+            result: "unknown"
+        },
+        {
+            metabolite: "calcium",
+            range: 100,
+            result: "unknown"
+        },
+        {
+            metabolite: "co2",
+            range: 25,
+            result: "unknown"
+        },
+        {
+            metabolite: "chloride",
+            range: 100,
+            result: "unknown"
+        },
+        {
+            metabolite: "creatinine",
+            range: 10,
+            result: "unknown"
+        },
+        {
+            metabolite: "glucose",
+            range: 800,
+            result: "unknown"
+        },
+        {
+            metabolite: "potassium",
+            range: 40,
+            result: "unknown"
+        },
+        {
+            metabolite: "sodium",
+            range: 140,
+            result: "unknown"
+        },
+        {
+            metabolite: "bilirubin",
+            range: 8,
+            result: "unknown"
+        },
+        {
+            metabolite: "protein",
+            range: 60,
+            result: "unknown"
+        },
+        {
+            metabolite: "alt",
+            range: 30,
+            result: "unknown"
+        },
+        {
+            metabolite: "alp",
+            range: 40,
+            result: "unknown"
+        },
+        {
+            metabolite: "ast",
+            range: 30,
+            result: "unknown"
+        }
+    ]
